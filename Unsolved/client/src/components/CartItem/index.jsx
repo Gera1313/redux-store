f959c567c39b3884650a2b1aa0f988080a25825c
Mark Anthony <touya1313@hotmail.com>
2023-11-03 04:32:26 -0600

    cartitem

--- Unsolved/client/src/components/CartItem/index.jsx
+++ Unsolved/client/src/components/CartItem/index.jsx
@@ -1,39 +1,25 @@
-import { useStoreContext } from "../../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { useDispatch } from 'react-redux';
+import { removeFromCart, updateCartQuantity } from '../../redux/actions';
 
 const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
 
-  const [, dispatch] = useStoreContext();
-
-  const removeFromCart = item => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id
-    });
-    idbPromise('cart', 'delete', { ...item });
-
+  const removeFromCartHandler = (itemId) => {
+    dispatch(removeFromCart(itemId));
+    idbPromise('cart', 'delete', { _id: itemId });
   };
 
-  const onChange = (e) => {
-    const value = e.target.value;
-    if (value === '0') {
-      dispatch({
-        type: REMOVE_FROM_CART,
-        _id: item._id
-      });
-      idbPromise('cart', 'delete', { ...item });
+  const updateQuantityHandler = (itemId, purchaseQuantity) => {
+    if (purchaseQuantity === 0) {
 
+      removeFromCartHandler(itemId);
     } else {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: item._id,
-        purchaseQuantity: parseInt(value)
-      });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
 
+      dispatch(updateCartQuantity(itemId, purchaseQuantity));
+      idbPromise('cart', 'put', { ...item, purchaseQuantity });
     }
-  }
+  };
 
   return (
     <div className="flex-row">
@@ -51,12 +37,12 @@ const CartItem = ({ item }) => {
             type="number"
             placeholder="1"
             value={item.purchaseQuantity}
-            onChange={onChange}
+            onChange={(e) => updateQuantityHandler(item._id, parseInt(e.target.value))}
           />
           <span
             role="img"
             aria-label="trash"
-            onClick={() => removeFromCart(item)}
+            onClick={() => removeFromCartHandler(item._id)}
           >
             🗑️
           </span>
