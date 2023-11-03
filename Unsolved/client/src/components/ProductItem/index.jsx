22bd83fc411d900295a10fb188190bb57af92254
Mark Anthony <touya1313@hotmail.com>
2023-11-03 03:17:32 -0600

    done with productitem index

--- Unsolved/client/src/components/ProductItem/index.jsx
+++ Unsolved/client/src/components/ProductItem/index.jsx
@@ -1,12 +1,11 @@
+// Already refactored to best of my knowledge
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-// import { useStoreContext } from "../../utils/GlobalState";
 import { useSelector, useDispatch } from 'react-redux';
-import { addToCart, updateCartQuantity } from '../../redux/actions';
+import { addToCart, updateCartQuantity } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  // const [state, dispatch] = useStoreContext();
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
 
@@ -22,21 +21,12 @@ function ProductItem(item) {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
     if (itemInCart) {
       dispatch(updateCartQuantity(_id, parseInt(itemInCart.purchaseQuantity) + 1));
-      // dispatch({
-      //   type: UPDATE_CART_QUANTITY,
-      //   _id: _id,
-      //   purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      // });
       idbPromise('cart', 'put', {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
       dispatch(addToCart({ ...item, purchaseQuantity: 1 }));
-      // dispatch({
-      //   type: ADD_TO_CART,
-      //   product: { ...item, purchaseQuantity: 1 }
-      // });
       idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
   }
