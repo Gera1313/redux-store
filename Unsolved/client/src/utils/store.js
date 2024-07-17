6501c8260a13bf608e28c483fecd03d70cd3989d
Mark Anthony <touya1313@hotmail.com>
2024-07-17 00:39:23 -0600

    something is displaying now

--- Unsolved/client/src/utils/store.js
+++ Unsolved/client/src/utils/store.js
@@ -1,16 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import rootReducer from './reducers';
-
-// const store = configureStore({
-//   reducer: rootReducer,
-//   initialState: {
-//     cart: [],
-//     cartOpen: false,
-//   },
-// });
-
-// export default store;
-// -----------------------------------------------------------
 // import { createStore } from 'redux';
 // import reducers from './reducers';
 
@@ -19,7 +6,7 @@
 // -----------------------------------------------------------
 
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers'; // Ensure your path to reducers is correct
+import rootReducer from './reducers';
 
 const store = configureStore({
   reducer: rootReducer
