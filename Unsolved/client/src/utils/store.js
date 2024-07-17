1e7ffced872c272829cb13dd9d39bed4a829aabc
Mark Anthony <touya1313@hotmail.com>
2024-07-16 23:55:31 -0600

    store.js

--- Unsolved/client/src/utils/store.js
+++ Unsolved/client/src/utils/store.js
@@ -1,12 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
+// import { configureStore } from '@reduxjs/toolkit';
+// import rootReducer from './reducers';
 
-const store = configureStore({
-  reducer: rootReducer,
-  initialState: {
-    cart: [],
-    cartOpen: false,
-  },
-});
+// const store = configureStore({
+//   reducer: rootReducer,
+//   initialState: {
+//     cart: [],
+//     cartOpen: false,
+//   },
+// });
 
-export default store;
+// export default store;
+
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+export default createStore(reducers);
