b50f73130f6a4651c6d11df72bba2b1e79e54e06
Mark Anthony <touya1313@hotmail.com>
2024-07-17 00:32:27 -0600

    fixed index.jsx

--- Unsolved/client/src/components/Cart/index.jsx
+++ Unsolved/client/src/components/Cart/index.jsx
@@ -5,15 +5,15 @@ import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { useSelector, useDispatch } from 'react-redux';
-import { toggleCart, addMultipleToCart } from '../../utils/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
   const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart);
+  const state = useSelector((state) => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
@@ -27,21 +27,21 @@ const Cart = () => {
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
-      dispatch(addMultipleToCart([...cart]));
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
     }
 
-    if (!cart.length) {
+    if (!state.cart.length) {
       getCart();
     }
-  }, [cart.length, dispatch]);
+  }, [state.cart.length, dispatch]);
 
   function toggleCart() {
-    dispatch(toggleCart());
+    dispatch({ type: TOGGLE_CART });
   }
 
   function calculateTotal() {
     let sum = 0;
-    cart.forEach((item) => {
+    state.cart.forEach((item) => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
@@ -50,7 +50,7 @@ const Cart = () => {
   function submitCheckout() {
     const productIds = [];
 
-    cart.forEach((item) => {
+    state.cart.forEach((item) => {
       for (let i = 0; i < item.purchaseQuantity; i++) {
         productIds.push(item._id);
       }
@@ -61,7 +61,7 @@ const Cart = () => {
     });
   }
 
-  if (!cart.length) {
+  if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
         <span role="img" aria-label="trash">
@@ -77,9 +77,9 @@ const Cart = () => {
         [close]
       </div>
       <h2>Shopping Cart</h2>
-      {cart.length ? (
+      {state.cart.length ? (
         <div>
-          {cart.map((item) => (
+          {state.cart.map((item) => (
             <CartItem key={item._id} item={item} />
           ))}
 
--- Unsolved/client/src/utils/store.js
+++ Unsolved/client/src/utils/store.js
@@ -10,8 +10,19 @@
 // });
 
 // export default store;
+// -----------------------------------------------------------
+// import { createStore } from 'redux';
+// import reducers from './reducers';
 
-import { createStore } from 'redux';
-import reducers from './reducers';
+// export default createStore(reducers);
 
-export default createStore(reducers);
+// -----------------------------------------------------------
+
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers'; // Ensure your path to reducers is correct
+
+const store = configureStore({
+  reducer: rootReducer
+});
+
+export default store;
